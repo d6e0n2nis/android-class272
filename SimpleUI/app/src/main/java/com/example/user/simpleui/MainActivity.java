021dc54ad556bb97cbd774d6251675ca95287c09
d6e0n2nis <d6e0n2nis@gmail.com>
2016-08-09 12:08:13 +0800

    add button drawtable file

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         text = text + "Order" + drink;
         textView.setText("text");
 
+        editText.setText("");
     }
 
     @Override
